ec92d63f5a9c8d46cf891e1bfe205fa89bc1e074
Roman <anufriev.roman@protonmail.com>
2023-11-23 22:08:09 +0300

    Increasing limit for item name

--- calcure/data.py
+++ calcure/data.py
@@ -146,7 +146,7 @@ class Collection:
 
     def add_item(self, item):
         """Add an item to the collection"""
-        if 100 > len(item.name) > 0 and item.name != "\[":
+        if 1000 > len(item.name) > 0 and item.name != "\[":
             self.items.append(item)
             self.changed = True
 
