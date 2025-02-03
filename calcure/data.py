9b81fc663ef5ffb97ad89cb6cdec7a2b35346577
Thomas Ramsauer <t.ramsauer@iggf.geo.uni-muenchen.de>
2025-02-03 11:18:42 +0100

    continue with next event if ValueError occurs.

--- calcure/data.py
+++ calcure/data.py
@@ -386,7 +386,7 @@ class RepeatedEvents(Events):
                 try:
                     rule = rrulestr(event.rrule, dtstart=dtstart)
                 except ValueError as e:
-                    print(e)
+                    continue
                 rset = rruleset()
                 rset.rrule(rule)
 
@@ -397,7 +397,7 @@ class RepeatedEvents(Events):
                         for exdate in exdates.dts:
                             exdate_dt = datetime.datetime.combine(exdate.dt, datetime.time.min, tzinfo=dtstart.tzinfo) if not isinstance(exdate.dt, datetime.datetime) else exdate.dt
                             rset.exdate(exdate_dt)
-                
+
                 # Create an event for each repetition and add to the list:
                 for date in list(rset)[1:]:
                     self.add_item(UserRepeatedEvent(event.item_id, date.year, date.month, date.day, event.name,
