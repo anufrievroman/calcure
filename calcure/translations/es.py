f700b95b848320821c3f4d15b2440e35938fc7f2
yoliveros <28690224+yoliveros@users.noreply.github.com>
2025-10-05 15:26:06 -0400

    translation file complete

--- calcure/translations/es.py
+++ calcure/translations/es.py
@@ -23,7 +23,7 @@ KEYS_CALENDAR = {
         "  a(A) ": "Agregar un evento (recurrente)",
         "   n   ": "Próximo mes (día)",
         "   p   ": "Mes anterior (día)",
-        "   x   ": "Elimine un evento",
+        "   x   ": "Eliminar un evento",
         "   r   ": "Renombrar un evento",
         "  m(M) ": "Mover evento (en este mes)",
         "  g(G) ": "Ir a un día específico (en este mes)",
@@ -47,7 +47,7 @@ KEYS_TODO = {
         "   r   ": "Renombra una tarea",
         "   s   ": "Alternar entre tarea y subtarea",
         "   .   ": "Alternar la privacidad de la tarea",
-        "  f(F) ": "Cambiar (remover) el límite de la tarea",
+        "  f(F) ": "Cambiar (remover) la fecha límite de la tarea",
         "   m   ": "Mover una tarea",
         "   C   ": "Importar tareas desde calcurse",
         }
@@ -64,52 +64,50 @@ MSG_EVENT_DONE    = "Marcar como terminado el evento número: "
 MSG_EVENT_RESET   = "Resetear el estatus para el evento número: "
 MSG_EVENT_DEL     = "Eliminar el evento número: "
 MSG_EVENT_REN     = "Renombrar el evento número: "
-MSG_NEW_TITLE     = "Ingrese un nuevo título: "
-MSG_EVENT_MV      = "Move event number: "
+MSG_NEW_TITLE     = "Ingresar un nuevo título: "
 MSG_EVENT_MV      = "Mover evento número: "
 MSG_EVENT_MV_TO   = "Mover evento a (YYYY/MM/DD): "
 MSG_EVENT_MV_TO_D = "Mover evento a: "
-MSG_EVENT_DATE    = "Ingrese la fecha: "
-MSG_EVENT_TITLE   = "Ingrese un titulo: "
+MSG_EVENT_DATE    = "Ingresar la fecha: "
+MSG_EVENT_TITLE   = "Ingresar un titulo: "
 MSG_EVENT_REP     = "Cuantas veces quiere repetir el evento: "
 MSG_EVENT_FR      = "¿Repetir el evento cada (d)ía, (w)semana, (m)mes o (y)año? "
 MSG_EVENT_IMP     = "¿Importar eventos desde Calcurse? (y/n)"
 MSG_EVENT_PRIVACY = "Alternar privacidad del evento número: "
 MSG_TM_ADD        = "Agregar/pausar temporizador por número de tarea: "
 MSG_TM_RESET      = "Remover temporizador para la tarea número: "
-MSG_TS_HIGH       = "Mark as high priority the task number: "
-MSG_TS_HIGH       = "Marcar como alta prioridad el número de la tarea: "
-MSG_TS_LOW        = "Mark as low priority the task number: "
-MSG_TS_RES        = "Reset status for the task number: "
-MSG_TS_DONE       = "Mark as done the task number: "
-MSG_TS_DEL        = "Delete task number: "
-MSG_TS_DEL_ALL    = "Really delete all tasks? (y/n)"
-MSG_TS_EDT_ALL    = "Do you confirm this action? (y/n)"
-MSG_TS_MOVE       = "Move task from number: "
-MSG_TS_MOVE_TO    = "Move task to number: "
-MSG_TS_EDIT       = "Edit task number: "
-MSG_TS_TOG        = "Toggle subtask number: "
-MSG_TS_SUB        = "Add subtask for task number: "
-MSG_TS_TITLE      = "Enter subtask: "
-MSG_TS_IM         = "Import tasks from Calcurse? (y/n)"
-MSG_TS_TW         = "Import tasks from Taskwarrior? (y/n)"
-MSG_TS_NOTHING    = "Nothing planned..."
-MSG_TS_PRIVACY    = "Toggle privacy of task number: "
-MSG_TS_DEAD_ADD   = "Add deadline for task number: "
-MSG_TS_DEAD_DEL   = "Remove deadline of the task number: "
-MSG_TS_DEAD_DATE  = "Add deadline on (YYYY/MM/DD): "
-MSG_WEATHER       = "Weather is loading..."
-MSG_ERRORS        = "Errors have occurred. See info.log in your config folder."
-MSG_INPUT         = "Incorrect input."
-MSG_GOTO          = "Go to date (YYYY/MM/DD): "
-MSG_GOTO_D        = "Go to date: "
+MSG_TS_HIGH       = "Marcar como alta prioridad la tarea número: "
+MSG_TS_LOW        = "Marcar como baja prioridad la tarea número: "
+MSG_TS_RES        = "Resetear el estatus para la tarea número: "
+MSG_TS_DONE       = "Marcar como terminada la tarea número: "
+MSG_TS_DEL        = "Eliminar la tarea número: "
+MSG_TS_DEL_ALL    = "¿Realmente quiere eliminar todas las tareas? (y/n)"
+MSG_TS_EDT_ALL    = "¿Confirmas esta acción? (y/n)"
+MSG_TS_MOVE       = "Mover tarea desde el número: "
+MSG_TS_MOVE_TO    = "Mover tarea al número: "
+MSG_TS_EDIT       = "Editar tarea número: "
+MSG_TS_TOG        = "Alternar subtarea número: "
+MSG_TS_SUB        = "Agregar subtarea para la tarea número: "
+MSG_TS_TITLE      = "Ingresar subtarea: "
+MSG_TS_IM         = "¿Importar tareas desde Calcurse? (y/n)"
+MSG_TS_TW         = "¿Importar tareas desde Taskwarrior? (y/n)"
+MSG_TS_NOTHING    = "Nada planificado..."
+MSG_TS_PRIVACY    = "Alternar privacidad de la tarea número: "
+MSG_TS_DEAD_ADD   = "Agregar fecha límite para la tarea número: "
+MSG_TS_DEAD_DEL   = "Remover fecha límite para la tarea número: "
+MSG_TS_DEAD_DATE  = "Agregar fecha límite en (YYY/MM/DD): "
+MSG_WEATHER       = "Cargando el clima..."
+MSG_ERRORS        = "Han ocurrido errores. Vea info.log en su carpeta config."
+MSG_INPUT         = "Entrada incorrecta."
+MSG_GOTO          = "Ir a la fecha (YYYY/MM/DD): "
+MSG_GOTO_D        = "Ir a la fecha: "
 
-CALENDAR_HINT     = "Space · Switch to journal   a · Add event  n/p · Change month   ? · All keybindings"
-CALENDAR_HINT_D   = "Space · Switch to journal   a · Add event  n/p · Change day   ? · All keybindings"
-JOURNAL_HINT      = "Space · Switch to calendar   a · Add task   d · Done   i · Important   ? · All keybindings"
+CALENDAR_HINT     = "Espacio · Ir al diario   a · Agregar evento  n/p · Cambiar mes   ? · Mostrar atajos"
+CALENDAR_HINT_D   = "Espacio · Ir al diario   a · Agregar evento  n/p · Cambiar día   ? · Mostrar atajos"
+JOURNAL_HINT      = "Espacio · Ir al calendario   a · Agregar tarea   d · Hecho   i · Importante   ? · Mostrar atajos"
 
-DAYS = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+DAYS = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
 DAYS_PERSIAN = ["SHANBEH", "YEKSHANBEH", "DOSHANBEH", "SESHANBEH", "CHAHARSHANBEH", "PANJSHANBEH", "JOMEH"]
 
-MONTHS = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+MONTHS = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
 MONTHS_PERSIAN = ["FARVARDIN", "ORDIBEHESHT", "KHORDAD", "TIR", "MORDAD", "SHAHRIVAR", "MEHR", "ABAN", "AZAR", "DEY", "BAHMAN", "ESFAND"]
